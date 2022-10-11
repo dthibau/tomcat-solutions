fe7da4f4ef69e39b34c21046e650ffa80aa48832
David THIBAU <david.thibau@gmail.com>
2022-10-11 22:01:50 +0200

    TP8 Accès distant JMX

--- tomcat.sh
+++ tomcat.sh
@@ -5,7 +5,7 @@ CATALINA_HOME=/home/dthibau/Formations/Tomcat/mywork/apache-tomcat-10.0.26
 export CATALINA_HOME
 JRE_HOME=/usr/lib/jvm/java-14-oracle/
 export JRE_HOME
-CATALINA_OPTS="-Xms512m -Xmx512m"
+CATALINA_OPTS="-Dcom.sun.management.jmxremote.port=7777 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=true -Dcom.sun.management.jmxremote.password.file=./jmxPasswords.txt -Dcom.sun.management.jmxremote.access.file=jmxAccess.txt -Xms512m -Xmx512m"
 export CATALINA_OPTS
 
 start() {
